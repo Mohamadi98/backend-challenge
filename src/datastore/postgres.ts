396bc6a467462f3e69694c0b5fbcec76bc2fdd56
Mohamadi98 <mohamadinaena23@gmail.com>
2026-06-22 03:09:20 +0300

    database connection

---
+++ src/datastore/postgres.ts
@@ -0,0 +1,22 @@
+import { Pool, QueryResult } from "pg";
+import dotenv from 'dotenv'
+
+dotenv.config()
+const connectionString = process.env.POSTGRES_CONNECTION_STRING
+console.log(connectionString)
+export class postgres {
+    private pool: Pool
+
+    constructor() {
+        this.pool = new Pool({
+            connectionString,
+            max: 20
+        })
+        this.pool.on('error', (error) => {
+            console.error('Postgres error: ', error)
+        })
+    }
+    public async query(query: string, params?: any[]): Promise<QueryResult> {
+        return this.pool.query(query, params)
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,32 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { postgres } from './datastore/postgres'
 
-const app = express()
-app.use(bodyParser.json())
-const requestLogger: express.RequestHandler = (req, _res, next) => {
-    console.log(req.method, req.path, " Body - ", req.body, " Params - ", req.params)
-    next()
-}
-app.use(requestLogger)
-app.get('/healthz', (_req, res) => {
-    res.send('OK')
-})
+(async ()=> {
+    const app = express()
+    const postgresClient = new postgres()
+    const requestLogger: express.RequestHandler = (req, _res, next)=> {
+        console.log(
+            req.method, req.path, " Body - ", req.body, " Params - ", req.params
+        )
+        next()
+    }
+    app.use(bodyParser.json())
+    app.use(requestLogger)
 
-app.listen(3000, () => {
-    console.log('Server running on PORT: 3000')
-})
+    app.get('/healthz', (_req, res) => {
+        res.send({'Status': 'OK'})
+    })
+
+    try {
+        await postgresClient.query('SELECT NOW()')
+        console.log('Database connected successfuly!')
+
+        app.listen(3000, () => {
+        console.log('Server running on PORT: 3000')
+    })
+    } catch (error) {
+        console.error('Database connection failed:', error)
+        process.exit(1)
+    }
+})()
